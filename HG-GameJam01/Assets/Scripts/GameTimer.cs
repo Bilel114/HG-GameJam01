e1bac657d390a30f231f4361b3dc94d7d33faf04
Bilel <bilelkharrat1141@gmail.com>
2025-09-23 22:30:50 +0100

    Added game timer logic and visuals

---
+++ HG-GameJam01/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    public float MaxTimer = 60;
+    [SerializeField] float _timer;
+    private float _timerRatio;
+    public float TimerRate = 1;
+    public bool EnableTimer = false;
+    public Image TimerUIMask;
+    public Image TimerUISprite;
+    public Animator HourglassAnimator;
+    public Color[] TimerColors = new Color[4];
+    public bool TimeEnded;
+
+    private void Start ()
+    {
+        StartTimer();
+    }
+
+    private void Update ()
+    {
+        if (EnableTimer)
+        {
+            if (_timer > 0)
+            {
+                _timer -= TimerRate * Time.deltaTime;
+                _timerRatio = _timer / MaxTimer;
+                TimerUIMask.fillAmount = _timerRatio;
+                if (_timerRatio > 0.75f)
+                {
+                    TimerUISprite.color = TimerColors[0];
+                }
+                else if (_timerRatio > 0.5f && _timerRatio <= 0.75f)
+                {
+                    TimerUISprite.color = TimerColors[1];
+                }
+                else if (_timerRatio > 0.25f && _timerRatio <= 0.5f)
+                {
+                    TimerUISprite.color = TimerColors[2];
+                }
+                else
+                {
+                    TimerUISprite.color = TimerColors[3];
+                } 
+            }
+            else
+            {
+                if (!TimeEnded)
+                {
+                    TimeEnded = true;
+                    OnTimeEnded();
+                }
+            }
+        }
+    }
+
+    public void StartTimer ()
+    {
+        EnableTimer = true;
+        _timer = MaxTimer;
+        HourglassAnimator.enabled = true;
+    }
+
+    public void DecreaseTimer (float time)
+    {
+        _timer -= time;
+    }
+
+    public void OnTimeEnded ()
+    {
+
+    }
+}
